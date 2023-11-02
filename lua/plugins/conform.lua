2032982d44e2af8fcb1eaa834caadcd3bc9b3070
Halloway <hamzaelhallaoui@gmail.com>
2023-11-02 18:45:27 +0100

    final config wokring

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,14 +1,15 @@
 return {
-	'stevearc/conform.nvim',
+	"stevearc/conform.nvim",
+	event = "BufWritePre",
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- Conform will run multiple formatters sequentially
-				python = { "isort",'autopep8', "black" },
+				python = { "isort", "autopep8", "black" },
 				-- Use a sub-list to run only the first available formatter
 				javascript = { { "prettierd", "prettier" } },
 			},
 		})
-	end
+	end,
 }
