bce29b00d50f1af1b7e8c3c579c7ce1341b8fe04
Halloway <hamzaelhallaoui@gmail.com>
2023-11-17 19:20:52 +0100

    Added some keymaps modifications

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,52 +1,59 @@
 -- Navigate between split windows with Cmd+h, j, k, l
-vim.api.nvim_set_keymap("n", "<S-h>", "<C-W>h", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-j>", "<C-W>j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-k>", "<C-W>k", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-l>", "<C-W>l", { noremap = true, silent = true })
+-- keymaps.lua
+local map = vim.api.nvim_set_keymap
+local keymap=vim.keymap.set
+-- Mappings for Magma
+map("n", "<LocalLeader>r", ":MagmaEvaluateOperator<CR>", { noremap = true, silent = true, expr = true })
+map("n", "<LocalLeader>rr", ":MagmaEvaluateLine<CR>", { noremap = true, silent = true })
+map("x", "<LocalLeader>r", ":<C-u>MagmaEvaluateVisual<CR>", { noremap = true, silent = true })
+map("n", "<LocalLeader>rc", ":MagmaReevaluateCell<CR>", { noremap = true, silent = true })
+map("n", "<LocalLeader>rd", ":MagmaDelete<CR>", { noremap = true, silent = true })
+map("n", "<LocalLeader>ro", ":MagmaShowOutput<CR>", { noremap = true, silent = true })
+map("n", "<S-h>", "<C-W>h", { noremap = true, silent = true })
+map("n", "<S-j>", "<C-W>j", { noremap = true, silent = true })
+map("n", "<S-k>", "<C-W>k", { noremap = true, silent = true })
+map("n", "<S-l>", "<C-W>l", { noremap = true, silent = true })
 -- Generals Keymaps
 -- Left Explorer Togglu
-vim.keymap.set("n", "<leader>e", ":Lexplore15<CR>")
+keymap("n", "<leader>e", ":Lexplore15<CR>")
 -- Move text up and down
-vim.keymap.set("n", "<M-k>", ":m .-2<CR>==", {})
-vim.keymap.set("n", "<M-j>", ":m .+1<CR>==", {})
+keymap("n", "<M-k>", ":m .-2<CR>==", {})
+keymap("n", "<M-j>", ":m .+1<CR>==", {})
 -- Move block text up and down
-vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv", {})
-vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv", {})
+keymap("v", "<M-j>", ":m '>+1<CR>gv=gv", {})
+keymap("v", "<M-k>", ":m '<-2<CR>gv=gv", {})
 
 -- Send the senction of code to the terminal
-vim.api.nvim_set_keymap("n", "<leader>ss", [[:call jukit#send#section(0)<CR>]], { noremap = true, silent = true })
+map("n", "<leader>ss", [[:call jukit#send#section(0)<CR>]], { noremap = true, silent = true })
 --Send the line of code to the terminal
-vim.api.nvim_set_keymap("n", "<leader>sa", [[:call jukit#send#line()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>cc", [[:call jukit#send#selection()<cr>]], { noremap = true, silent = true })
+map("n", "<leader>sa", [[:call jukit#send#line()<CR>]], { noremap = true, silent = true })
+map("v", "<leader>cc", [[:call jukit#send#selection()<cr>]], { noremap = true, silent = true })
 --gitlazy keymap
-vim.api.nvim_set_keymap("n", "<Leader>gg", [[:LazyGit<CR>]], { noremap = true, silent = true })
+map("n", "<Leader>gg", [[:LazyGit<CR>]], { noremap = true, silent = true })
 --telescope
 -- See `:help telescope.builtin`
-vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
-vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
-vim.keymap.set("n", "<leader>/", function()
+keymap("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
+keymap("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
+keymap("n", "<leader>/", function()
 	-- You can pass additional configuration to telescope to change theme, layout, etc.
 	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 		winblend = 10,
 		previewer = false,
 	}))
 end, { desc = "[/] Fuzzily search in current buffer" })
-vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
-vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
-vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
-vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
-vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
+keymap("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
+keymap("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
+keymap("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+keymap("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
+keymap("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+keymap("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+keymap("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-vim.keymap.set("n", "<leader>o", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
--- vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format the code' })
+keymap("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+keymap("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+keymap("n", "<leader>o", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+keymap("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+-- keymap('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format the code' })
 -- Key mapping to trigger the Format command
-vim.api.nvim_set_keymap("n", "<Leader>f", [[:Format<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "n", "nzzzv", { noremap = true, silent = true })
-
-
-
+map("n", "<Leader>f", [[:Format<CR>]], { noremap = true, silent = true })
+map("n", "n", "nzzzv", { noremap = true, silent = true })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,23 +1,23 @@
 -- mapleader linked to space
-vim.g.mapleader = ' ' -- Set <Leader> to the Space key
+vim.g.mapleader = " " -- Set <Leader> to the Space key
 --write code here
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = " "
 -- vim.g.python3_host_prog = '/usr/local/bin/python'
-vim.o.clipboard='unnamedplus'
-vim.cmd('set relativenumber')
+vim.o.clipboard = "unnamedplus"
+vim.cmd("set relativenumber")
 vim.o.termguicolors = true
 -- Enable break indent
 vim.o.breakindent = true
 -- Save undo history
 vim.o.undofile = true
 -- Flashes the Yanked highlighted part
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
 		vim.highlight.on_yank()
 	end,
 	group = highlight_group,
-	pattern = '*',
+	pattern = "*",
 })
 -- Case-insensitive searching UNLESS \C or capital in search
 vim.o.ignorecase = true
@@ -25,11 +25,13 @@ vim.o.smartcase = true
 
 vim.o.hlsearch = false
 -- Send the block of code to the terminal
-vim.api.nvim_set_keymap('n', '<leader>sa', [[:call jukit#send#section(0)<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>sa", [[:call jukit#send#section(0)<CR>]], { noremap = true, silent = true })
 --Send the line of code to the terminal
-vim.api.nvim_set_keymap('n', '<leader>sl', [[:call jukit#send#line()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>sl", [[:call jukit#send#line()<CR>]], { noremap = true, silent = true })
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 -- Set the width of tabs and spaces
-vim.cmd('set shiftwidth=2')  -- Number of spaces per tab
-vim.cmd('set tabstop=2')     -- Number of spaces that a <Tab> in the file counts for
+vim.cmd("set shiftwidth=2") -- Number of spaces per tab
+vim.cmd("set tabstop=2") -- Number of spaces that a <Tab> in the file counts for
+vim.g.magma_automatically_open_output = false
+vim.g.magma_image_provider = "ueberzug"
