0ec52468eee4ed952749073e3b9b2916119341c9
Halloway <hamzaelhallaoui@gmail.com>
2024-03-21 17:53:46 +0100

    keymaps update, obsidian pluggin working,

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,22 +2,22 @@
 -- keymaps.lua
 local map = vim.api.nvim_set_keymap
 local keymap = vim.keymap.set
-map("n", "<S-h>", "<C-W>h", { noremap = true, silent = true })
-map("n", "<S-j>", "<C-W>j", { noremap = true, silent = true })
-map("n", "<S-k>", "<C-W>k", { noremap = true, silent = true })
-map("n", "<S-l>", "<C-W>l", { noremap = true, silent = true })
+map("n", "<C-h>", "<C-W>h", { noremap = true, silent = true })
+-- map("n", "<C-j>", "<C-W>j", { noremap = true, silent = true })
+-- map("n", "<C-k>", "<C-W>k", { noremap = true, silent = true })
+map("n", "<C-l>", "<C-W>l", { noremap = true, silent = true })
 -- Generals Keymaps
 -- Left Explorer Togglu
 -- keymap("n", "<leader>e", ":Lexplore15<CR>")
 -- Move text up and down
-keymap("n", "<M-k>", ":m .-2<CR>==", {})
-keymap("n", "<M-j>", ":m .+1<CR>==", {})
+keymap("n", "<S-j>", ":m .+1<CR>==", { noremap = true, silent = true})
+keymap("n", "<S-k>", ":m .-2<CR>==", { noremap = true, silent = true})
 -- Move block text up and down
-keymap("v", "<M-j>", ":m '>+1<CR>gv=gv", {})
-keymap("v", "<M-k>", ":m '<-2<CR>gv=gv", {})
+keymap("v", "<S-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true})
+keymap("v", "<S-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true})
 --Keymaps for nvim tree
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
--- Send the senction of code to the terminal
+-- Send the section of code to the terminal
 map("n", "<leader>ss", [[:call jukit#send#section(0)<CR>]], { noremap = true, silent = true })
 --Send the line of code to the terminal
 map("n", "<leader>sa", [[:call jukit#send#line()<CR>]], { noremap = true, silent = true })
@@ -37,7 +37,7 @@ keymap("n", "<leader>/", function()
 	}))
 end, { desc = "[/] Fuzzily search in current buffer" })
 keymap("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
-keymap("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
+keymap("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "[F]ind [F]iles" })
 keymap("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
 keymap("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
 keymap("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
@@ -51,7 +51,6 @@ keymap("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics l
 -- keymap('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format the code' })
 -- Key mapping to trigger the Format command
 map("x", "<leader>p", '"_dP', { noremap = true, silent = true })
-map("n", "<Leader>f", [[:Format<CR>]], { noremap = true, silent = true })
 map("n", "<Leader>f", [[:Format<CR>]], { noremap = true, silent = true })
 --keep the cursor centered when scrolling
 keymap("n", "<C-d>", "<C-d>zz", {})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -25,7 +25,8 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- Case-insensitive searching UNLESS \C or capital in search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
+-- Conceal level for obsidian
+vim.opt.conceallevel = 1
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
